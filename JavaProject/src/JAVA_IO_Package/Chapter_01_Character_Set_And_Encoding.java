0823bd1486c5fdce002ccd8f5218b4fef6bc5a13
bigongja <dnrms47@naver.com>
2025-01-22 16:29:46 +0900

    CharacterSet과 Encoding의 개념

---
+++ JavaProject/src/JAVA_IO_Package/Chapter_01_Character_Set_And_Encoding.java
@@ -0,0 +1,14 @@
+package JAVA_IO_Package;
+
+// Java I/O 패키지를 공부하기 전에 반드시 알아야 할 내용
+public class Chapter_01_Character_Set_And_Encoding {
+	
+	/** 각 나라의 언어를 표현하기 위해 Character Set이 필요하다.
+	 *  영어의 경우, 1Byte를 사용하며 1Byte는 8Bit이며 256개 표현할 수 있고 첫 번째 비트의 값은 0으로 되어있다.
+	 *  한글의 경우, 2Byte가 필요하며 2Byte는 16Bit이며 65536개 표현을 할 수 있고, 첫 번째 비트의 값은 1로 두 번째 Byte까지 사용한다는 명시를 해준다.
+	 *  
+	 *  입력 하는 데이터는 Character Set으로 설정하지만, 컴퓨터가 읽을 때는 Encodig 작업을 해줘야 한다.
+	 *  1Byte 일 때, [1Byte]Character Set은 ASCII 코드로 표현되어 있으며
+	 *  2Byte 일 때, [1Byte]Character Set( ASCII 코드 ) + [1Byte]다른 Character Set을 가진다. 
+	 * */
+}
--- JavaProject/src/Lambda/Lambda_StreamAPI/Lambda_StreamAPI_flatMap.java
+++ JavaProject/src/Lambda/Lambda_StreamAPI/Lambda_StreamAPI_flatMap.java
@@ -1,5 +1,31 @@
 package Lambda.Lambda_StreamAPI;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.io.
+
+// 문자열 리스트에서 단어 추출
 public class Lambda_StreamAPI_flatMap {
 
+	public static void main(String[] args) {
+		
+		List<String> sentences = Arrays.asList("Hello world", "Java Stream API", "Lambda expressions");
+		
+		/** flatMap() 메서드는 스트림의 각 요소를 다른 스트림으로 변환한 후, 이 모든 스트림을 하나의 스트림으로 병합하는 메서드
+		 *  - 기능 : 중첩된 스트림 구조를 평탄화(flatten)하여 단일 스트림으로 만들 때 사용됩니다.
+		 *  - 매개변수 : Function<T, Stream<R>> 인터페이스를 구현한 객체를 매개변수로 받습니다.
+		 *             여기서 T는 스트림의 요소 타입이고, R은 변환될 스트림의 요소 타입
+		 *             
+		 *  - 람다식 표현 sentence -> Stream.of(sentence.split(" "))는 Function<String, Stream<String>> 인터페이스를 구현한 익명 객체
+		 *  - 람다 표현식에서 sentence는 스트림의 각 문장을 의미
+		 *  - sentence.split(" ")는 문장을 공백을 기준으로 나누어 단어 배열을 생성합니다.
+		 *  - Stream.of()는 단어 배열을 스트림으로 변환 합니다.
+		 * */
+		List<String> words = sentences.stream()
+									  .flatMap(sentence -> Stream.of(sentence.split(" "))) // 문장을 단어 하나 하나를 스트림으로 변환
+									  .collect(Collectors.toList());
+		System.out.println(words);
+	}
 }
