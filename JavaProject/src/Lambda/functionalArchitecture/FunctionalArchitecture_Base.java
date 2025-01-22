7494027bb9e85a242fd18e58448abc33efbdc541
bigongja <dnrms47@naver.com>
2025-01-22 17:06:22 +0900

    Java Functional_Architecture 공부해야될 내용

---
+++ JavaProject/src/Lambda/functionalArchitecture/FunctionalArchitecture_Base.java
@@ -0,0 +1,7 @@
+package Lambda.functionalArchitecture;
+/** 자바 Functional Architecture
+ *  자바에서 Funcational_Architecture를 이용하면 동시성 문제를 해결할 수 있다. [ 이유 : 각각 다른 새로운 객체를 생성해서 사용하기 때문에 ] 
+ */
+public class FunctionalArchitecture_Base {
+	
+}
