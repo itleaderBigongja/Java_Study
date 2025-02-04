f5a784eb19c311ffa1464aa2bd267fd718180510
bigongja <dnrms47@naver.com>
2025-02-04 18:00:03 +0900

    빅 에디안과 리틀 에디안 설명 추가

--- JavaProject/src/JAVA_IO_Package/File_IO_Package/IO_File_Reader.java
+++ JavaProject/src/JAVA_IO_Package/File_IO_Package/IO_File_Reader.java
@@ -3,10 +3,12 @@ package JAVA_IO_Package.File_IO_Package;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class IO_File_Reader {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		// try-with-resource 구문: 자원을 자동으로 해제해주는 구문
 		// 사용 이유: BufferedReader와 FileReader를 사용한 후 자동으로 
 		//          close()를 호출하여 자원 누수를 방지하기 위해 사용
@@ -36,5 +38,26 @@ public class IO_File_Reader {
 			// IOException: I/O 작업 중 발생하는 예외를 처리하는 클래스
 			// 사용 이유: 파일 입출력 시 발생할 수 있는 예외를 처리하기 위해 사용
 		} 
+		
+//		String str = "가";
+		for(byte a : "가".getBytes("UTF-8")) {
+			System.out.print(Integer.toHexString(a));
+		}
+		
+		try {
+			// ISO-8859-1  1
+			// US-ASCII    1
+			// UTF-8       3
+			// UTF-16      4
+			// UTF-16BE    2
+			// UTF-16LE    2
+		
+			
+			System.out.println("가".getBytes("UTF-16BE")); 
+			System.out.println("가".getBytes("UTF-16LE"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- JavaProject/src/Thread/thread_7_HighRank/Thread_08_thenAcceptBoth_Method.java
+++ JavaProject/src/Thread/thread_7_HighRank/Thread_08_thenAcceptBoth_Method.java
@@ -1,5 +1,41 @@
 package Thread.thread_7_HighRank;
-
+/** thenAcceptBoth 메서드
+ *  thenAcceptBoth 메서드는 CompletableFuture 클래스에서 제공하는 비동기 처리 메시드 중 하나이다.
+ *  이 메서드는 두 개의 CompletableFuture가 모두 완료되면, 각각의 결과를 받아서 특정 작업을 실행하는 데 사용된다.
+ *  두 작업의 결과를 함께 사용해야 하지만, 새로운 결과를 반환하지 않을 경우에 적합하다.
+ *  
+ *  기능
+ *  < 두 작업 완료 대기 >
+ *  ㅇ thenAcceptBoth 메서드는 두 개의 CompletableFuture 작업이 모두 성공적으로 완료될 때까지 기다린다.
+ *  
+ *  < 결과 소비 >
+ *  ㅇ 두 작업이 모두 완료되면, 각각의 결과를 받아서 BiConsumer를 통해 특정 작업을 실행한다.
+ *  
+ *  < 결과 반환 없음 >
+ *  ㅇ thenAcceptBoth 메서드는 새로운 CompletableFuture<Void>를 반환한다.
+ *    결과 소비하지만, 새로운 결과를 생성하지 않고 단순히 작업을 수행하는데 목적이 있다.
+ *    
+ *  < 비동기 처리 >
+ *  ㅇ thenAcceptBoth에 제공된 BiConsumer는 비동기적으로 실행된다. 즉, thenAcceptBoth 메서드를 호출한 즉시 콜백이 실행되는 것이 아니라
+ *    두 CompletableFuture가 완료된 후 별도의 스레드에서 실행된다.
+ *    
+ *  특징
+ *  < 두 결과 소비 >
+ *  ㅇ 두 개의 CompletableFuture 결과를 모두 받아서 소비한다.
+ *  
+ *  < BiConsumer 사용 >
+ *  ㅇ 결과를 처리하기 위해 java.util.function.BiConsumer 인터페이스를 사용한다.
+ *     BiConsumer는 두 개의 입력 값을 받아 들이고 작업을 수행하지만, 값을 반환하지 않는 함수형 인터페이스이다.
+ *     
+ *  < 비동기 동작 >
+ *  ㅇ 이전 작업 완료 후 콜백이 실행되므로 비동기적으로 동작한다.
+ *  
+ *  < 예외 처리 >
+ *  ㅇ 두 CompletableFuture 중 하나라도 예외로 완료되면, thenAcceptBoth 메서드도 예외로 완료되며, 콜백은 실행되지 않는다.
+ *  
+ *  
+ * */
 public class Thread_08_thenAcceptBoth_Method {
 
+	
 }
