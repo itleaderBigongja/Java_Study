4d4469d4cff4e36f99623b7ca1a8b082c2ec679a
bigongja <dnrms47@naver.com>
2025-02-05 23:11:29 +0900

    스레드 : thenAcceptBoth() 메서드

--- JavaProject/src/Thread/thread_7_HighRank/Thread_08_thenAcceptBoth_Method.java
+++ JavaProject/src/Thread/thread_7_HighRank/Thread_08_thenAcceptBoth_Method.java
@@ -1,4 +1,8 @@
 package Thread.thread_7_HighRank;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 /** thenAcceptBoth 메서드
  *  thenAcceptBoth 메서드는 CompletableFuture 클래스에서 제공하는 비동기 처리 메시드 중 하나이다.
  *  이 메서드는 두 개의 CompletableFuture가 모두 완료되면, 각각의 결과를 받아서 특정 작업을 실행하는 데 사용된다.
@@ -31,11 +35,47 @@ package Thread.thread_7_HighRank;
  *  ㅇ 이전 작업 완료 후 콜백이 실행되므로 비동기적으로 동작한다.
  *  
  *  < 예외 처리 >
- *  ㅇ 두 CompletableFuture 중 하나라도 예외로 완료되면, thenAcceptBoth 메서드도 예외로 완료되며, 콜백은 실행되지 않는다.
- *  
- *  
- * */
+ *  ㅇ 두 CompletableFuture 중 하나라도 예외로 완료되면, thenAcceptBoth 메서드도 예외로 완료되며, 콜백은 실행되지 않는다.  
+ **/
 public class Thread_08_thenAcceptBoth_Method {
-
 	
+	public static void main(String[] args) {
+		
+		// 첫 번째 : CompletableFuture: 5초 후, "Hello" 반환
+		CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+			return "Hello";
+		});
+		
+		// 두 번째 : CompletableFuture: 3초 후, "World" 반환
+		CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+			try {
+				Thread.sleep(3000);
+			} catch (Exception e) {
+				Thread.currentThread().interrupt();
+			}
+			return "World";
+		});
+		
+		// 두 CompletableFuture가 모두 완료되면 결과를 받아 출력
+		CompletableFuture<Void> combinedFuture = future1.thenAcceptBoth(future2, (s1, s2) -> {
+			
+			System.out.println("Future1 result: " + s1);
+			System.out.println("Future2 result: " + s2);
+			System.out.println("Combined result: " + s1 + " " + s2);
+		});
+		
+		// combinedFuture가 완료될 때까지 대기( 선택 사항 )
+		try {
+			combinedFuture.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Done");
+	}
 }
